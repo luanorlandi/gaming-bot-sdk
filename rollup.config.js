5bc1ffbf36d0fff419e4e06caa31555af39ad268
Luan Orlandi <orlandi.luan@gmail.com>
2018-06-20 00:12:21 -0300

    Adjust packages, scripts and eslint

--- rollup.config.js
+++ rollup.config.js
@@ -5,7 +5,6 @@ import json from 'rollup-plugin-json';
 export default {
   input: 'src/compete/contest/code-of-kutulu/index.js',
   output: {
-    file: 'bundle.js',
     format: 'cjs',
   },
   plugins: [
