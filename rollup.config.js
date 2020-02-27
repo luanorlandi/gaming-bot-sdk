14e502ef554742124f6637e2a2b64a43ffba5554
Luan Orlandi <orlandi.luan@gmail.com>
2020-02-26 22:42:02 -0300

    Upgrade rollup plugin typescript

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
 import json from 'rollup-plugin-json';
-import typescript from 'rollup-plugin-typescript';
+import typescript from '@rollup/plugin-typescript';
 
 export default {
   input: 'bot/index.js',
