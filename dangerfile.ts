7ecece06ed233b4e4b8c4c97ea6522a219dfce8c
Luan Orlandi <orlandi.luan@gmail.com>
2020-07-09 19:12:30 -0300

    ci: fix danger

--- dangerfile.ts
+++ dangerfile.ts
@@ -2,9 +2,13 @@
 import { danger, warn } from 'danger';
 import { codeCoverage } from 'danger-plugin-code-coverage';
 
-codeCoverage();
+codeCoverage([{
+  title: '# Coverage',
+  ignoreCoveragePattern: ['.test.', '.snap'],
+  coverageFilesPath: 'packages/core/coverage/coverage-final.json',
+}]);
 
 // No PR is too small to include a description of why you made a change
-if (danger.github.pr.body.length < 10) {
+if (danger?.github?.pr?.body?.length < 10) {
   warn('Please include a description of your PR changes.');
 }
