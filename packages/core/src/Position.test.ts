06036addfde6460abcfe22f66f9a7b64341d9af8
Luan Orlandi <orlandi.luan@gmail.com>
2020-07-10 23:50:39 -0300

    test: rollback file used to test ci

---
+++ packages/core/src/Position.test.ts
@@ -0,0 +1,8 @@
+import Position from './Position';
+
+test('distance of (0, 0) to (3, 4)', () => {
+  const positionA = new Position(0, 0);
+  const positionB = new Position(3, 4);
+  const distance = positionA.distanceTo(positionB);
+  expect(distance).toEqual(5);
+});
